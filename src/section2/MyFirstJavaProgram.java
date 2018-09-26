03b1501947dcc7b0382384262a6081cd3a853fea
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-26 13:50:00 -0700

    Eric

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,7 +7,15 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
+		Robot r2d2=new Robot();
+		r2d2.setSpeed(15);
+		r2d2.penDown();
+
+		for(int i=0;i<8;i++) {
+			r2d2.move(120);
+			r2d2.turn(360/8);
+		}
+
 	
 		
 		
